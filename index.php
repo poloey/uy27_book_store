535dafbac94a58eb0a27f123e0df74a58c009920
= <=>
2018-03-07 22:08:14 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php session_start();?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		<?php
+			include("includes/head.inc.php");
+		?>
+</head>
+
+<body>
+			<!-- start header -->
+				<div id="header">
+					<div id="menu">
+						<?php
+							include("includes/menu.inc.php");
+						?>
+					</div>
+				</div>
+			<!-- end header -->
+			
+			<!-- start page -->
+
+				<div id="page">
+					<!-- start content -->
+					<div id="content">
+						<div class="post">
+							<h1 class="title">Welcome to 
+							<?php 
+								if(isset($_SESSION['status']))
+								{
+									echo $_SESSION['unm']; 
+								}
+								else
+								{	
+									echo 'Book Store';
+								}
+							?>
+							</h1>
+							<p>user: admin password: admin</p>
+							<div class="entry">
+								<br>
+								
+								<br>		
+								
+								<br><br>
+								<h2>Simple book store for education purpose only </h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore vitae quisquam quod rem dolore quam accusamus harum eum, nam at nostrum, explicabo deserunt libero amet modi itaque, et repellendus iure!
+								</p>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+					<!-- end content -->
+					
+					<!-- start sidebar -->
+					<div id="sidebar">
+							<?php
+								include("includes/search.inc.php");
+							?>
+					</div>
+					<!-- end sidebar -->
+					<div style="clear: both;">&nbsp;</div>
+				</div>
+			<!-- end page -->
+			
+			<!-- start footer -->
+				<div id="footer">
+							<?php
+								include("includes/footer.inc.php");
+							?>
+				</div>
+			<!-- end footer -->
+</body>
+</html>
